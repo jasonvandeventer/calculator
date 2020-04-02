05d595738c2a046f69effa7d4d6fe1c934fb3d40
Jason VanDeventer <jasonvandeventer@gmail.com>
2020-04-02 18:17:42 -0500

    Rearranged buttons and added missing ones

--- calculator.py
+++ calculator.py
@@ -26,38 +26,40 @@ button_dec = Button(root, text=str("."), padx=41.5, pady=20,
                     command=button_add)
 button_mult = Button(root, text=str("*"), padx=39, pady=20, command=button_add)
 button_subt = Button(root, text=str("-"), padx=39, pady=20, command=button_add)
-button_plus = Button(root, text=str("-"), padx=39, pady=20, command=button_add)
+button_plus = Button(root, text=str("+"), padx=39, pady=20, command=button_add)
 button_tot = Button(root, text=str("="), padx=37, pady=20, command=button_add)
+button_div = Button(root, text=str("\\"), padx=37, pady=20, command=button_add)
 button_inv = Button(root, text=str("+/-"), padx=34, pady=20,
                     command=button_add)
 button_clr = Button(root, text=str("Clear"), padx=77, pady=20,
                     command=button_add)
-button_bksp = Button(root, text=str("Bkspc"), padx=77, pady=20,
+button_bksp = Button(root, text=str("Bkspc"), padx=28, pady=20,
                      command=button_add)
 
 # Place buttons
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
-button_mult.grid(row=1, column=3)
+button_7.grid(row=2, column=0)
+button_8.grid(row=2, column=1)
+button_9.grid(row=2, column=2)
+button_mult.grid(row=2, column=3)
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
-button_subt.grid(row=2, column=3)
+button_4.grid(row=3, column=0)
+button_5.grid(row=3, column=1)
+button_6.grid(row=3, column=2)
+button_subt.grid(row=3, column=3)
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
-button_plus.grid(row=3, column=3)
+button_1.grid(row=4, column=0)
+button_2.grid(row=4, column=1)
+button_3.grid(row=4, column=2)
+button_plus.grid(row=4, column=3)
 
-button_inv.grid(row=4, column=0)
-button_0.grid(row=4, column=1)
-button_dec.grid(row=4, column=2)
-button_tot.grid(row=4, column=3)
+button_inv.grid(row=5, column=0)
+button_0.grid(row=5, column=1)
+button_dec.grid(row=5, column=2)
+button_tot.grid(row=5, column=3)
 
-button_clr.grid(row=5, column=0, columnspan=2)
-button_bksp.grid(row=5, column=2, columnspan=2)
+button_clr.grid(row=1, column=0, columnspan=2)
+button_bksp.grid(row=1, column=2)
+button_div.grid(row=1, column=3)
 
 
 root.mainloop()
