2d363ee4789a92c414ad9c1fbd67db518bae0725
Jason VanDeventer <jasonvandeventer@gmail.com>
2020-04-02 18:24:54 -0500

    Changed function name

--- calculator.py
+++ calculator.py
@@ -7,34 +7,39 @@ e = Entry(root, width=50, borderwidth=5, textvariable=1)
 e.grid(row=0, column=0, columnspan=5, padx=10, pady=10, ipady=3)
 
 
-def button_add():
+def button_click(number):
     return
 
 
 # Create buttons
-button_1 = Button(root, text=1, padx=40, pady=20, command=button_add)
-button_2 = Button(root, text=2, padx=40, pady=20, command=button_add)
-button_3 = Button(root, text=3, padx=40, pady=20, command=button_add)
-button_4 = Button(root, text=4, padx=40, pady=20, command=button_add)
-button_5 = Button(root, text=5, padx=40, pady=20, command=button_add)
-button_6 = Button(root, text=6, padx=40, pady=20, command=button_add)
-button_7 = Button(root, text=7, padx=40, pady=20, command=button_add)
-button_8 = Button(root, text=8, padx=40, pady=20, command=button_add)
-button_9 = Button(root, text=9, padx=40, pady=20, command=button_add)
-button_0 = Button(root, text=0, padx=40, pady=20, command=button_add)
+button_1 = Button(root, text=1, padx=40, pady=20, command=button_click)
+button_2 = Button(root, text=2, padx=40, pady=20, command=button_click)
+button_3 = Button(root, text=3, padx=40, pady=20, command=button_click)
+button_4 = Button(root, text=4, padx=40, pady=20, command=button_click)
+button_5 = Button(root, text=5, padx=40, pady=20, command=button_click)
+button_6 = Button(root, text=6, padx=40, pady=20, command=button_click)
+button_7 = Button(root, text=7, padx=40, pady=20, command=button_click)
+button_8 = Button(root, text=8, padx=40, pady=20, command=button_click)
+button_9 = Button(root, text=9, padx=40, pady=20, command=button_click)
+button_0 = Button(root, text=0, padx=40, pady=20, command=button_click)
 button_dec = Button(root, text=str("."), padx=41.5, pady=20,
-                    command=button_add)
-button_mult = Button(root, text=str("*"), padx=39, pady=20, command=button_add)
-button_subt = Button(root, text=str("-"), padx=39, pady=20, command=button_add)
-button_plus = Button(root, text=str("+"), padx=39, pady=20, command=button_add)
-button_tot = Button(root, text=str("="), padx=37, pady=20, command=button_add)
-button_div = Button(root, text=str("\\"), padx=37, pady=20, command=button_add)
+                    command=button_click)
+button_mult = Button(root, text=str("*"), padx=39, pady=20, 
+                     command=button_click)
+button_subt = Button(root, text=str("-"), padx=39, pady=20, 
+                     command=button_click)
+button_plus = Button(root, text=str("+"), padx=39, pady=20, 
+                     command=button_click)
+button_tot = Button(root, text=str("="), padx=37, pady=20, 
+                    command=button_click)
+button_div = Button(root, text=str("\\"), padx=37, pady=20, 
+                    command=button_click)
 button_inv = Button(root, text=str("+/-"), padx=34, pady=20,
-                    command=button_add)
+                    command=button_click)
 button_clr = Button(root, text=str("Clear"), padx=77, pady=20,
-                    command=button_add)
+                    command=button_click)
 button_bksp = Button(root, text=str("Bkspc"), padx=28, pady=20,
-                     command=button_add)
+                     command=button_click)
 
 # Place buttons
 button_7.grid(row=2, column=0)
